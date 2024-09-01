4df8a1fd1ca003dd94b2308d7a7abde825a438b5
Leonardo Farroco <leonardo.farroco@nextroll.com>
2024-09-01 16:03:54 -0300

    remove generated ts file

--- test/todo/output/gemini-main.js
+++ test/todo/output/gemini-main.js
@@ -1,37 +1,29 @@
+"use strict";
 const todos = [
-  { name: 'buy bread', dueTo: '01/01/2025', completed: true },
-  { name: 'buy milk', dueTo: '02/02/2026', completed: false },
-  { name: 'buy eggs', dueTo: '03/03/2027', completed: false },
-  { name: 'buy cheese', dueTo: '04/04/2028', completed: false },
-  { name: 'buy ham', dueTo: '05/05/2029', completed: false },
-  { name: 'buy wine', dueTo: '06/06/2030', completed: false },
+    { name: 'buy bread', dueTo: new Date('2025-01-01'), completed: true },
+    { name: 'buy milk', dueTo: new Date('2026-02-02'), completed: false },
+    { name: 'buy eggs', dueTo: new Date('2027-03-03'), completed: false },
+    { name: 'buy cheese', dueTo: new Date('2028-04-04'), completed: false },
+    { name: 'buy yogurt', dueTo: new Date('2029-05-05'), completed: false },
+    { name: 'buy water', dueTo: new Date('2030-06-06'), completed: false },
 ];
-
-function printTodoItem(todoItem) {
-  const completedSymbol = todoItem.completed ? 'x' : ' ';
-  console.log(`[${completedSymbol}] ${todoItem.name} - due to ${todoItem.dueTo}`);
+function printTodoItem(todo) {
+    const completedSymbol = todo.completed ? '[x]' : '[ ]';
+    console.log(`${completedSymbol} ${todo.name} - due to ${todo.dueTo.toISOString().slice(0, 10)}`);
 }
-
-function markTodoItemAsCompleted(todoItem) {
-  todoItem.completed = true;
+function markTodoItemAsCompleted(todo) {
+    todo.completed = true;
 }
-
-function markTodoItemAsIncomplete(todoItem) {
-  todoItem.completed = false;
+function markTodoItemAsIncomplete(todo) {
+    todo.completed = false;
 }
-
 todos.forEach(printTodoItem);
-
 console.log('====');
-
 markTodoItemAsCompleted(todos[1]);
 printTodoItem(todos[1]);
-
 console.log('====');
-
 markTodoItemAsIncomplete(todos[0]);
 printTodoItem(todos[0]);
-
 console.log('====');
-
 console.log('Goodbye!');
+//# sourceMappingURL=gemini-main.js.map
