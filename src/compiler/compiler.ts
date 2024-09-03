1cc20c0248375a7df409157f5743f2c7161e2cfd
Leonardo Farroco <leonardo.farroco@nextroll.com>
2024-09-03 10:46:19 -0300

    update prompt

--- src/compiler/compiler.ts
+++ src/compiler/compiler.ts
@@ -1,7 +1,6 @@
 import * as fs from "fs";
 
 import { getEnv, getFiles } from "../utils";
-import { supportedLanguageCode, supportedLanguages } from "../constants";
 import { openai } from "../api/openai";
 import { gemini } from "../api/gemini";
 import { ollama } from "../api/ollama";
@@ -10,14 +9,12 @@ export default async function compile({
   outputPath,
   sourceDir,
   dotEnvFilePath,
-  targetLanguage,
   aiProvider,
   model,
 }: {
   outputPath: string;
   sourceDir: string;
   dotEnvFilePath: string;
-  targetLanguage: string;
   aiProvider: string;
   model: string;
 }) {
@@ -40,68 +37,83 @@ export default async function compile({
 
   //console.log("Compiled files:", compiledFiles);
 
-
-  const maybeLanguageName = supportedLanguages[targetLanguage.toLowerCase()];
-  const languageName = maybeLanguageName ? maybeLanguageName : targetLanguage;
-
   //TODO: have different examples for different languages
-  //TODO: if generating java, use "class" instead of "function"
 
   const prompt = `
 
-Your task is to generate a program that implements the functionality
+Your task is to generate a TypeScript program that implements the functionality
 described in a series of virtual files. 
+
 Each file name is delimited by double equal signs (==).
+
 These virtual files are from "Verbo", a programming language that allows describing
 software with natural language.
+The intention of the language is allowing the user to create simple, self-contained software.
+
 Software described in Verbo has the following properties:
 - A single mutable state that holds all the data
-- Events that can change the state
+- All symbols are unique (regardless of case or clojure)
 - Constants
 - Functions
 - Objects
 - Types
 - Ports that allow the software to interact with the external world
-All Verbo files are written in Markdown format.
-The output of the program should be a single file that implements the described functionality.
-A single "main" function or class should be generated that will run the software.
+
+All Verbo files are written in the Markdown format.
+
+The generated code of the program should be a single file that implements the described functionality.
+
+A single "main" function should be generated that will run the software.
+
 Avoid importing external libraries.
-That "main" function or class should accept the following parameters:
-- An object with parameters that will be used to configure the software
-- An object that will be used to initialize the state
-- An object with port function that can be used by the software
-The intention of the language is allowing the user to create simple, self-contained software.
-One example of such main function that could be generated is:
-export function main(config, initialState, ports) {
-  const url = config.baseUrl + "/users";
-  const state = initialState;
+
+That "main" function should accept the following parameters:
+- An initial state (null if no state is used)
+- An object with port functions that can be used by the software
+
+One example of such generated code:
+
+export type User = {
+  name: string;
+}
+export function main(
+  initialState: State,
+  ports:{
+    print: (message: string) => void,
+    updateUser: (user: User) => void,
+    getUserFromDB: (id: string) => User
+):number {
+  ...
+  const state = initializeState(initialState);
+  ...
   // using ports:
   ports.print("Hello, world!");
   ports.updateUser({ name: "Alice" });
-  ports.sendEmail({ to: "", subject: "", body: "" });
-Using those parameters is not obligatory.
-If the target language is object-oriented, "main" will be a class.
-If the target language is functional, "main" will be a function.
-Multiparadigm languages will have the choice of using a class or a function.
+  const user = ports.getUserFromDB("123");
+  ...
+  return 22
+}
+
+The local state is mutable.
+All other side effects (logging, requests, etc.) should be done using the provided ports.
+
 The generated "main" function should be exposed to make it importable by other code, so that the user
 may use it in their own codebase.
+Types used in the parameters should be exported as well.
 
 Starting from the file "main.md", generate code that implements the described software.
 The generated code should represent a single ouput file that can be run in the target language.
 
+All functions/constants/variables should be contained inside in the main function.
+
 If the code description says something like "... generate n items", or "generate n random items",
 it means that you should generate data that fits that context.
 
-As the Verbo language is language-agnostice, you may use any language constructs
-that will be appropriate to implement the described functionality.
-For example, if the description defines an "object", you are free to use a class, 
-struct or dictionary depending on what will be more convenient in the target language.
-The target programming language is ${languageName}.
 The response should come as a single block of code.
 It is very important that the generated response contains only code.
 If you want to add an explanation, use comments.
-The code should not be wrapped in backticks.
-After the code is generated, it will be fed into a formatter and linter, so ensure
+The code should *not* be wrapped in backticks.
+After the code is generated, it will be fed into a formatter and linter, to ensure
 that no illegal artifacts are present.
 `;
   const submitPrompt = `${prompt}\n\n${compiledFiles}`;
@@ -152,11 +164,14 @@ that no illegal artifacts are present.
     text = text.split("\n").slice(0, -1).join("\n");
   }
 
-  console.log(`Writing ${aiProvider}-main.${targetLanguage} to the output folder.`);
+  console.log(`Writing ${aiProvider}-ts-main.ts to the output folder.`);
 
-  fs.writeFileSync(`${outputPath}/${aiProvider}-main.${targetLanguage}`, text);
+  fs.writeFileSync(`${outputPath}/${aiProvider}-ts-main.ts`, text);
 
   console.log(
     'Your code is ready in the target output folder.'
   );
+
+  // TODO: feed into formatter and linter, on error, feed it another prompt
+
 }
